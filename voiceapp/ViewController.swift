361307a4cd44a9ce12db85b8a128cd6c247a3011
janvi kachchhi007 <janukachchhi2@gmail.com>
2023-06-21 12:34:36 +0530

    UI DESIGN

--- voiceapp/ViewController.swift
+++ voiceapp/ViewController.swift
@@ -9,15 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var playButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        playButton.layer.cornerRadius = 5
+        playButton.layer.cornerRadius = 25
         playButton.layer.masksToBounds = true
+        progressBar.progress = 0.0
+        
     }
 
     @IBAction func playButtonAction(_ sender: Any) {
-        naviget()
+        
+        var time = Timer()
+        var a : Float = 0.0
+        time = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { time in
+            a+=0.01
+            self.progressBar.progress = a
+            if self.progressBar.progress == 1.0
+            {
+                self.progressBar.progress = 0.0
+            }
+        })
+        naviget()3.description
     }
     func naviget()
     {
